3c5e5789a8df5987cd4dbda6cbfe29fd37118f41
SallyHabib <sally.hanen@docusign.com>
2021-05-14 03:52:44 +0200

    feat: starting camera and taking photos

---
+++ App.tsx
@@ -0,0 +1,182 @@
+import {StatusBar} from 'expo-status-bar'
+import React from 'react'
+import {StyleSheet, Text, View, TouchableOpacity, Alert, ImageBackground, Image} from 'react-native'
+import {Camera} from 'expo-camera'
+let camera: Camera
+export default function App() {
+  const [startCamera, setStartCamera] = React.useState(false)
+  const [previewVisible, setPreviewVisible] = React.useState(false)
+  const [capturedImage, setCapturedImage] = React.useState<any>(null)
+  const [cameraType, setCameraType] = React.useState(Camera.Constants.Type.back)
+
+  const startCameraFunc = async () => {
+    const {status} = await Camera.requestPermissionsAsync()
+    if (status === 'granted') {
+      setStartCamera(true)
+    } else {
+      Alert.alert('Access denied')
+    }
+  }
+  const takePicture = async () => {
+    const photo: any = await camera.takePictureAsync()
+    setPreviewVisible(true)
+    setCapturedImage(photo)
+  }
+  
+  
+  return (
+    <View style={styles.container}>
+      {startCamera ? (
+        <View
+          style={{
+            flex: 1,
+            width: '100%'
+          }}
+        >
+          {previewVisible && capturedImage ? (
+            <CameraPreview photo={capturedImage} />
+          ) : (
+            <Camera
+              type={cameraType}
+              flashMode={'off'}
+              style={{flex: 1}}
+              ref={(r) => {
+                camera = r
+              }}
+            >
+              <View
+                style={{
+                  flex: 1,
+                  width: '100%',
+                  backgroundColor: 'transparent',
+                  flexDirection: 'row'
+                }}
+              >
+                <View
+                  style={{
+                    position: 'absolute',
+                    left: '5%',
+                    top: '10%',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between'
+                  }}
+                >
+                </View>
+                <View
+                  style={{
+                    position: 'absolute',
+                    bottom: 0,
+                    flexDirection: 'row',
+                    flex: 1,
+                    width: '100%',
+                    padding: 20,
+                    justifyContent: 'space-between'
+                  }}
+                >
+                  <View
+                    style={{
+                      alignSelf: 'center',
+                      flex: 1,
+                      alignItems: 'center'
+                    }}
+                  >
+                    <TouchableOpacity
+                      onPress={takePicture}
+                      style={{
+                        width: 70,
+                        height: 70,
+                        bottom: 0,
+                        borderRadius: 50,
+                        backgroundColor: '#fff'
+                      }}
+                    />
+                  </View>
+                </View>
+              </View>
+            </Camera>
+          )}
+        </View>
+      ) : (
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: '#fff',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <TouchableOpacity
+            onPress={startCameraFunc}
+            style={{
+              width: 130,
+              borderRadius: 4,
+              backgroundColor: '#14274e',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: 40
+            }}
+          >
+            <Text
+              style={{
+                color: '#fff',
+                fontWeight: 'bold',
+                textAlign: 'center'
+              }}
+            >
+              Take picture
+            </Text>
+          </TouchableOpacity>
+        </View>
+      )}
+
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+const CameraPreview = ({photo, retakePicture, savePhoto}: any) => {
+  return (
+    <View
+      style={{
+        backgroundColor: 'transparent',
+        flex: 1,
+        width: '100%',
+        height: '100%'
+      }}
+    >
+      <ImageBackground
+        source={{uri: photo && photo.uri}}
+        style={{
+          flex: 1
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            padding: 15,
+            justifyContent: 'flex-end'
+          }}
+        >
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between'
+            }}
+          >
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  )
+}
--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# BuddyWise-challenge
+# Buddywise React Native Code Challenge
+
+## Objective
+Your task is to implement a simple app, where the user can enter IP address of a digital camera and stream it to the phone.
+
+Simply, fork this repo and build on top of the current codebase (`expo start` to build and run).
+When you are done with your solution, please share a link to your repo via email: yigit@buddywise.co
+
+## Required Features
+* As a User I should be able to enter the IP address of a digital camera and stream it.
+* As a User I should be able to watch the stream in both portrait and landscape mode.
+* As a User I should be able to disconnect from the camera and stop streaming it.
+
+## Things to keep in mind 🚨
+* Achieving required functionality and good code quality are both important in this challange.
+* Your code will be evaluated based on: code structure, programming best practices, legibility. (and *not* based on the extra functionality implemented). 
+* The git commit history (and git commit messages) will also be evaluated.
+
+Using third-party libraries is totally ok, and even encouraged, if you clearly state them.
+
+## Some publicly avaiable IP addresses
+Here are some publicly available IP addresses you can use for this challenge. (However you don't have to use these, as long as they are accessible by us, you can use any camera source)
+
+- http://84.82.29.229:8080/mjpg/video.mjpg
+- http://138.16.162.37:80/mjpg/video.mjpg
+- http://138.16.162.33:80/mjpg/video.mjpg
+- http://91.150.61.77:80/mjpg/video.mjpg
+
+Best of luck 🍀
